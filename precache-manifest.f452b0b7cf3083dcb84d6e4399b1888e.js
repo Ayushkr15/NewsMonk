17ac9535097a11bd077c6ee3dbd6de0837a154d2
Ayush Kumar Sinha <ayushkr1322@gmail.com>
2022-05-31 09:01:26 +0530

    Updates

---
+++ precache-manifest.f452b0b7cf3083dcb84d6e4399b1888e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4cc6d6283b654ab59be",
+    "url": "/NewsMonk/static/css/main.14d79011.chunk.css"
+  },
+  {
+    "revision": "e4cc6d6283b654ab59be",
+    "url": "/NewsMonk/static/js/main.e4cc6d62.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/NewsMonk/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "1129272bd196e3b1c340",
+    "url": "/NewsMonk/static/js/2.1129272b.chunk.js"
+  },
+  {
+    "revision": "8d5f3ee10f8d0ff020ed",
+    "url": "/NewsMonk/static/js/runtime~main.8d5f3ee1.js"
+  },
+  {
+    "revision": "cf9176f8be82e47a15e8a67fdfc24adb",
+    "url": "/NewsMonk/static/media/logoMonk.cf9176f8.png"
+  },
+  {
+    "revision": "67f942e95b39b66b5ce9025725a96ee0",
+    "url": "/NewsMonk/index.html"
+  }
+];
